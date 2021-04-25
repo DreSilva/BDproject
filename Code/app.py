a45e031dd3c4ed855aa96554b7b924d559ecd1eb
Joao Cruz <joao.scruz2000@gmail.com>
2021-04-25 12:38:32 +0100

    Login and Register Done

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,4 @@
 from configparser import ConfigParser
-
 from flask import Flask, jsonify, request
 import psycopg2
 import jwt
@@ -43,15 +42,8 @@ def hello_world():
 
 @app.route('/user/<username>,<email>,<password>', methods=['GET'])
 def register(username, email, password):
-    message = {"user": username, "pass": password, "mail": email}
-    return jsonify(message)
-
-
-@app.route('/user/<username>,<password>', methods=['PUT'])
-def login(username, password):
     conn = None
 
-    # warnings.filterwarnings("ignore")
     try:
         params = getDBConfigs()
 
@@ -60,7 +52,39 @@ def login(username, password):
 
         # create a cursor ( a cursor is the command that "talks" with the database")
         cur = conn.cursor()
-        isLogin = True  # mudar isto para checkar se existe
+        cur.execute("Begin Transaction")
+        cur.execute("Insert into pessoa(username,email,password,admin,banned) values(%s,%s,%s,false,false )",
+                    (username, email, password))
+        cur.execute("Select userid from pessoa where username=%s and password=%s", (username, password))
+        id = cur.fetchall()
+        cur.execute("commit")
+        message = {"userId": id[0]}
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        if isinstance(error, psycopg2.errors.UniqueViolation):
+            message = {"Code": 409, "error": "User or email already exists"}
+            return jsonify(message)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+@app.route('/user/<username>,<password>', methods=['PUT'])
+def login(username, password):
+    conn = None
+    try:
+        params = getDBConfigs()
+
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)  # creates connection with the data base
+
+        # create a cursor ( a cursor is the command that "talks" with the database")
+        cur = conn.cursor()
+        cur.execute("Select * from pessoa where username=%s and password=%s", (username, password))
+        isLogin = cur.fetchall()
         if isLogin:
             token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
                                app.config['SECRET_KEY'], algorithm="HS256")
@@ -70,9 +94,12 @@ def login(username, password):
         else:
             cur.close()
             conn.commit()
-            return "ERROR"  # por codigo de erro aqui
+            message = {"Code": 403, "error": "Wrong user or password"}
+            return jsonify(message)
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
     finally:
         if conn is not None:
             conn.close()
@@ -81,13 +108,13 @@ def login(username, password):
 
 @app.route('/leilao/<artigoId>', methods=['GET'])
 def criarLeilao(artigoId):
+    # Copiar isto para saber se o user tem token ou nao
     l, code = token_required(request.args.get('token'))
     if code == 403:
         return l
     else:
         user = l['user']
-    # mudar isto para criar com a BD
-    return artigoId
+    # fazer o resto aqui
 
 
 if __name__ == '__main__':
